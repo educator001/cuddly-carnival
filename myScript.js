a7a6b3be0d525478b06e6a6e2b0051f9fe294863
Middle Tree Tutor <matthewfrantes@middletree.org>
2019-11-10 15:15:40 -0800

    Various changes.

--- myScript.js
+++ myScript.js
@@ -1,6 +1,23 @@
-var d, message = "Hello, world!";
+var d, message = "Hello, world!", position = 0;
 
 function myFunction() {
   d = new Date();
   document.getElementById("test-id").innerHTML = d;
+}
+
+function goForward() {
+  position++;
+  if (position > 12) {
+    position = 0;
+  }
+  document.getElementById("position-display").innerHTML = position;
+  document.getElementById("pitch-deck").className = "playing";
+}
+
+function goBack() {
+  position--;
+  if (position < 0) {
+    position = 12;
+  }
+  document.getElementById("position-display").innerHTML = position;
 }
